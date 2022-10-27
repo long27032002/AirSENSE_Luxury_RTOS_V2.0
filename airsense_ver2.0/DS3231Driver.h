a4db2e5d32efaac7f57ef73a40f76c398c4e6052
NgDKhanh <Khanh1piecesky@gmail.com>
2022-10-27 23:02:24 +0700

    Update DS3231Driver.h

--- airsense_ver2.0/DS3231Driver.h
+++ airsense_ver2.0/DS3231Driver.h
@@ -1,5 +1,9 @@
 #include "RTClib.h"
 #include <ESP32Ping.h>
+#include "log.h"
+
+/* Wifi status */
+enum WIFI_status {WIFI_IS_CONNECTED, WIFI_UNCONNECTED, WIFI_PING_CONNECTED};
 
 const char* remote_host_string = "www.google.com";
 RTC_DS3231 realTime;
@@ -8,9 +12,11 @@ RTC_DS3231 realTime;
 /**
  * @brief	Khoi tao module thoi gian thuc
  *
- * @return  None
+ * @return  WIFI_PING_CONNECTED 2: Wifi da ket noi va ping duong dan "www.google.com"
+ * @return  WIFI_IS_CONNECTED   1: Wifi da ket noi nhung ping duong dan khac "www.google.com"
+ * @return  WIFI_UNCONNECTED    0: Wifi khong ket noi
  */
-void DS3231_init()
+uint32_t DS3231_init()
 {
 	realTime.begin();									// khoi dong module RTC
 	if (WiFi.status() == wl_status_t::WL_CONNECTED)		// kiem tra co ket noi wifi
@@ -21,12 +27,16 @@ void DS3231_init()
 			timeClient.update();				
 			uint32_t epochTime_u32 = timeClient.getEpochTime();
 			realTime.adjust(DateTime(epochTime_u32));			// Set the date and flip the Oscillator Stop Flag
-			Serial.println("Updatetime DS3231....");
+			LOG_PRINT_NOTIFICATION("Updatetime DS3231....");
+			return WIFI_PING_CONNECTED;							// Wifi da ket noi va ping duong dan "www.google.com"
 		}
+		return WIFI_IS_CONNECTED;								// Wifi da ket noi nhung ping duong dan khac "www.google.com"
 	}
-#ifdef	DEBUG_SERIAL
-	Serial.println(realTime.now().toString("YYYY-MMM-DD hh:mm:ss")); 		// in thoi gian hien tai ra Serial
-#endif
+	return WIFI_UNCONNECTED;									// Wifi khong ket noi
+	//LOG_PRINT_NONE(getDateTime("YYYY-MMM-DD hh:mm:ss"));
+//#ifdef	DEBUG_SERIAL
+//	LOG_PRINT_NOTIFICATION(getDateTime("YYYY-MMM-DD hh:mm:ss")); 		// in thoi gian hien tai ra Serial
+//#endif
 }
 
 
@@ -41,12 +51,12 @@ bool DS3231_getData()
 	if (realTime.now().isValid())		// kiem tra thoi gian co hop le
 	{
 #ifdef  DEBUG_SERIAL
-	Serial.println(realTime.now().toString("hh:mm DD-MMM-YY"));		// in thoi gian hien tai ra Serial
+	LOG_PRINT_NOTIFICATION(getDateTime("hh:mm DD-MMM-YY"));		// in thoi gian hien tai ra Serial
 #endif
 		return true;
 	} else {
 #ifdef  DEBUG_SERIAL
-	Serial.println("------ *** CAN'T GET TIME *** -----");
+	LOG_PRINT_ERROR("------ *** CAN'T GET TIME *** -----");
 #endif
 		return false;
 	}
